15425264ad509de059d73a25aadf12a8e128b8f1
darsana-dileep <darsanadileep98@gmail.com>
2022-04-22 16:25:31 +0530

    First release of Xpay Project

---
+++ xpay/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class XpayConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'xpay'
---
+++ xpay/models.py
@@ -0,0 +1,15 @@
+from django.contrib.auth.models import User
+from django.db import models
+
+
+# Create your models here.
+
+class Merchant(models.Model):
+    auth_user_id = models.OneToOneField(User, on_delete=models.CASCADE)
+    business = models.CharField(max_length=30)
+    phone = models.IntegerField()
+    email = models.CharField(max_length=20)
+    city = models.CharField(max_length=20)
+    busi = models.CharField(max_length=20)
+    location = models.CharField(max_length=20)
+    website = models.CharField(max_length=100)
